6091a80aa97cfea37fee8d92e64f1616f63c5642
chayanbajaj <45946285+chayanbajaj@users.noreply.github.com>
2024-06-07 16:05:00 +0530

    adds code to handle next line

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,8 +1,10 @@
 function add(numbers: string): number {
-  const withoutWhiteSpaces = numbers.replace(/\s+/g, "");
-  let stringArray: string[] = withoutWhiteSpaces.split(",");
+  const withoutWhiteSpaces = numbers.replace(/[^\S\n]+/g, "");
+  const finalString = withoutWhiteSpaces.replace(/\n/g, ',');
+  let stringArray: string[] = finalString.split(",");
+  console.log('stringArray', stringArray);
   let integerArray: number[] = stringArray.map(Number);
-  if (withoutWhiteSpaces === "") return 0;
+  if (finalString === "") return 0;
   else if (integerArray.length === 1) return integerArray[0];
   else return integerArray.reduce((acc, curr) => acc + curr, 0);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 async function main() {
   const { add } = await import("./calculator");
-  console.log(add("1000,2000,3000,4000"));
+  console.log(add("1\n2,3"));
 }
 
 main();
