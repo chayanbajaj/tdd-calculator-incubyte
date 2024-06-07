b334984daf83ddf258cab38d83d878a913e665ba
chayanbajaj <45946285+chayanbajaj@users.noreply.github.com>
2024-06-07 16:51:55 +0530

    adds code for delimeters

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,8 +1,16 @@
 function add(numbers: string): number {
+  let delimeter = ",";
   const withoutWhiteSpaces = numbers.replace(/[^\S\n]+/g, "");
-  const finalString = withoutWhiteSpaces.replace(/\n/g, ',');
-  let stringArray: string[] = finalString.split(",");
-  console.log('stringArray', stringArray);
+  let finalString = withoutWhiteSpaces;
+  if (finalString.startsWith("//")) {
+    const indexOfNewLine = finalString.indexOf("\n");
+    if (indexOfNewLine !== -1) {
+      delimeter = finalString.substring(2, indexOfNewLine);
+      finalString = finalString.substring(indexOfNewLine + 1);
+    }
+  }
+  finalString = finalString.replace(/\n/g, delimeter);
+  let stringArray: string[] = finalString.split(delimeter);
   let integerArray: number[] = stringArray.map(Number);
   if (finalString === "") return 0;
   else if (integerArray.length === 1) return integerArray[0];
