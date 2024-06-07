394c0c1daa4149215aacd50e08ea465d99f57c4c
chayanbajaj <45946285+chayanbajaj@users.noreply.github.com>
2024-06-07 16:04:39 +0530

    adds test to handle next line

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -19,5 +19,8 @@ describe("String Calculator", () => {
       expect(add("1000,2000,3000,4000")).toBe(10000);
     });
 
+    it("should handle new line", () => {
+      expect(add("1\n2,3")).toBe(6);
+    });
   });
 });
