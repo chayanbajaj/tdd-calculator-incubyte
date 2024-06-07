85d2bfb6f0bbd740f76656f02cd4dc7384e99400
chayanbajaj <45946285+chayanbajaj@users.noreply.github.com>
2024-06-07 15:40:50 +0530

    adds test for adding two numbers

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -6,9 +6,14 @@ describe("String Calculator", () => {
     it("should return 0 when input is empty", () => {
       expect(add("")).toBe(0);
     });
-    
+
     it('should return 1 when input is "1"', () => {
       expect(add("1")).toBe(1);
     });
+    
+    it('should return 6 when input is "1,5"', () => {
+      expect(add("1,5")).toBe(6);
+    });
+
   });
 });
