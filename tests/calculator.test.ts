63b7650c5088282839efbee5fa8e3c813de387f8
chayanbajaj <45946285+chayanbajaj@users.noreply.github.com>
2024-06-07 15:38:27 +0530

    adds test for single number

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -2,8 +2,13 @@ import { add } from "../src/calculator";
 
 describe("String Calculator", () => {
   describe("add method", () => {
+
     it("should return 0 when input is empty", () => {
       expect(add("")).toBe(0);
+    });
+    
+    it('should return 1 when input is "1"', () => {
+      expect(add("1")).toBe(1);
     });
   });
 });
